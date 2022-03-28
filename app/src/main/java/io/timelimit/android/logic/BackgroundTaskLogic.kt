2824d18db6077dacd0c5b3d91362117aeff0afab
Jonas Lochmann <git@inkompetenz.org>
2022-03-28 02:00:00 +0200

    Fix category title in the notification when limits are disabled

--- app/src/main/java/io/timelimit/android/logic/BackgroundTaskLogic.kt
+++ app/src/main/java/io/timelimit/android/logic/BackgroundTaskLogic.kt
@@ -618,9 +618,12 @@ class BackgroundTaskLogic(val appLogic: AppLogic) {
                 ): AppStatusMessage {
                     val handling = categoryHandlingCache.get(categoryId)
 
+                    val titlePrefix = getCategoryTitle(categoryId) + " - "
+
                     return if (handling.areLimitsTemporarilyDisabled) {
                         buildStatusMessageWithCurrentAppTitle(
                                 text = appLogic.context.getString(R.string.background_logic_limits_disabled),
+                                titlePrefix = titlePrefix,
                                 titleSuffix = suffix,
                                 appPackageName = appPackageName,
                                 appActivityToShow = appActivityToShow
@@ -628,7 +631,7 @@ class BackgroundTaskLogic(val appLogic: AppLogic) {
                     } else if (handling.remainingTime == null) {
                         buildStatusMessageWithCurrentAppTitle(
                                 text = appLogic.context.getString(R.string.background_logic_no_timelimit),
-                                titlePrefix = getCategoryTitle(categoryId) + " - ",
+                                titlePrefix = titlePrefix,
                                 titleSuffix = suffix,
                                 appPackageName = appPackageName,
                                 appActivityToShow = appActivityToShow
@@ -649,7 +652,7 @@ class BackgroundTaskLogic(val appLogic: AppLogic) {
                                     TimeTextUtil.pauseIn(remainingSessionDuration.toInt(), appLogic.context)
                                 else
                                     TimeTextUtil.remaining(realRemainingTimeDefault.toInt() ?: 0, appLogic.context),
-                                titlePrefix = getCategoryTitle(categoryId) + " - ",
+                                titlePrefix = titlePrefix,
                                 titleSuffix = suffix,
                                 appPackageName = appPackageName,
                                 appActivityToShow = appActivityToShow
