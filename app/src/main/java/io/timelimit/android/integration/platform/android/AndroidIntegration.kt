1de799e5d511d5e1fde09392dc2e5835d8465569
Jonas Lochmann <git@inkompetenz.org>
2022-07-11 02:00:00 +0200

    Update dependencies

--- app/src/main/java/io/timelimit/android/integration/platform/android/AndroidIntegration.kt
+++ app/src/main/java/io/timelimit/android/integration/platform/android/AndroidIntegration.kt
@@ -243,7 +243,7 @@ class AndroidIntegration(context: Context): PlatformIntegration(maximumProtectio
     override fun setAppStatusMessage(message: AppStatusMessage?) {
         if (lastAppStatusMessage != message) {
             lastAppStatusMessage = message
-            appStatusMessageChannel.offer(message)
+            appStatusMessageChannel.trySend(message)
         }
     }
 
