152e479d4c2a44b625e652371f7780dd8bfcc6b8
Jonas Lochmann <git@inkompetenz.org>
2022-11-28 01:00:00 +0100

    Allow blocking the private dns settings

--- app/src/main/java/io/timelimit/android/integration/platform/android/AndroidFeatures.kt
+++ app/src/main/java/io/timelimit/android/integration/platform/android/AndroidFeatures.kt
@@ -18,16 +18,27 @@ package io.timelimit.android.integration.platform.android
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.UserManager
 import io.timelimit.android.R
 import io.timelimit.android.integration.platform.PlatformFeature
 
 object AndroidFeatures {
     private const val FEATURE_ADB = "adb"
+    private const val FEATURE_CONFIG_PRIVATE_DNS = "dns"
 
     fun applyBlockedFeatures(features: Set<String>, policyManager: DevicePolicyManager, admin: ComponentName): Boolean {
-        if (features.contains(FEATURE_ADB)) policyManager.addUserRestriction(admin, UserManager.DISALLOW_DEBUGGING_FEATURES)
-        else policyManager.clearUserRestriction(admin, UserManager.DISALLOW_DEBUGGING_FEATURES)
+        fun apply(feature: String, restriction: String) {
+            if (features.contains(feature)) policyManager.addUserRestriction(admin, restriction)
+            else policyManager.clearUserRestriction(admin, restriction)
+        }
+
+        apply(FEATURE_ADB, UserManager.DISALLOW_DEBUGGING_FEATURES)
+
+        if (VERSION.SDK_INT >= VERSION_CODES.Q) {
+            apply(FEATURE_CONFIG_PRIVATE_DNS, UserManager.DISALLOW_CONFIG_PRIVATE_DNS)
+        }
 
         return true
     }
@@ -39,6 +50,15 @@ object AndroidFeatures {
             id = FEATURE_ADB,
             title = context.getString(R.string.dummy_app_feature_adb)
         ))
+
+        if (VERSION.SDK_INT >= VERSION_CODES.Q) {
+            result.add(
+                PlatformFeature(
+                    id = FEATURE_CONFIG_PRIVATE_DNS,
+                    title = context.getString(R.string.dummy_app_feature_dns)
+                )
+            )
+        }
 
         return result
     }
