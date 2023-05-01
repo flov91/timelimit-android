c03b43452a8e2933f3fcf87e4bbb0239f2f356b9
Jonas Lochmann <git@inkompetenz.org>
2023-05-01 02:00:00 +0200

    Fix unstable packageNameToCategoryId in AddAppsModel

--- app/src/main/java/io/timelimit/android/ui/manage/category/apps/add/AddAppsModel.kt
+++ app/src/main/java/io/timelimit/android/ui/manage/category/apps/add/AddAppsModel.kt
@@ -111,7 +111,7 @@ class AddAppsModel(application: Application): AndroidViewModel(application) {
                         defaultCategory != null && parentCategories.contains(defaultCategory.category.id)
                     val packageNameToCategoryId =
                         userRelatedData.categoryApps
-                            .filter { it.appSpecifier.deviceId == null }
+                            .filter { it.appSpecifier.activityName == null && it.appSpecifier.deviceId == null }
                             .associateBy { it.appSpecifier.packageName }
 
                     installedApps.filter { app ->
